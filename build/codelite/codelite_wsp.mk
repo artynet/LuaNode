04672d52ef2185ff7c58c9078f0d673b5e326334
Arturo Rinaldi <arty.net2@gmail.com>
2016-01-12 12:51:45 +0100

    removing existing build directory

--- build/codelite/codelite_wsp.mk
+++
@@ -1,8 +0,0 @@
-.PHONY: clean All
-
-All:
-	@echo ----------Building project:[ LuaNode - Debug ]----------
-	@"mingw32-make.exe"  -j 8 -f "LuaNode.mk"
-clean:
-	@echo ----------Cleaning project:[ LuaNode - Debug ]----------
-	@"mingw32-make.exe"  -j 8 -f "LuaNode.mk" clean
